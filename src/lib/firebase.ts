821f03eac460fd6e4e465b27ce54e04e475d3aaa
RAGUL <ragul03062007@gmail.com>
2025-08-02 02:56:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: "shuddhair-health",
+  appId: "1:376315036170:web:2ac232fff49e01d72955f3",
+  storageBucket: "shuddhair-health.firebasestorage.app",
+  apiKey: "AIzaSyAFJ3eXUnFwuy68WTimEiZECEEFKat4kF4",
+  authDomain: "shuddhair-health.firebaseapp.com",
+  messagingSenderId: "376315036170"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
